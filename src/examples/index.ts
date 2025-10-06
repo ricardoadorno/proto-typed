b900722526b9a88e8ea414b8b58d772b0961c2cb
ricardo <ricardo.castrorc1998@gmail.com>
2025-10-06 11:16:01 -0300

    deploy: fix imports

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -1,19 +1,7 @@
 // Barrel export for all examples
 import login from './login';
-import dashboard from './dashboard';
-import mobileComplete from './mobile-complete';
-import namedElementsExample from './named-elements';
 import contactsAppExample from './contacts-app';
-import backNavigationExample from './back-navigation';
-import { mobileTest } from './mobile-test';
-import { buttonVariantsExample } from './button-variants';
-import listsExample from './lists';
-import metadataDemo from './metadata-demo';
-import { layoutModifiersExample } from './layout-modifiers';
-import componentPropsDemo from './component-props-demo';
-import testStructuralTokens from './test-structural-tokens';
 import customStylesDemo from './custom-styles-demo';
-import { fabTestExample } from './fab-test';
 import { navigatorTest } from './navigator-test';
 
 // Example configuration for UI
