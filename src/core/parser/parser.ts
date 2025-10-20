79d64add0a7a11bceee4abe7cc116d09d54e3f7f
Ricardo Castro Adorno <ricardo.castrorc1998@gmail.com>
2025-10-20 11:38:41 -0300

    revert: defazer problema da lista

--- src/core/parser/parser.ts
+++ src/core/parser/parser.ts
@@ -303,7 +303,6 @@ export class UiDslParser extends CstParser {
       this.CONSUME(ComponentInstanceWithProps, { LABEL: "componentName" });
     });
     this.OPTION2(() => {
-      this.CONSUME(Colon);
       this.CONSUME(Indent);
       this.AT_LEAST_ONE(() => {
         this.CONSUME(UnorderedListItem);
